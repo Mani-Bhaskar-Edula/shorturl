c2d1d09b0a964f3a1b805116c43c1f68ef054d80
mani-carboledger <mani.bhaskar@carboledger.com>
2024-09-09 15:40:10 +0530

    backend done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const {connectToMongoDB} = require("./connect");
+const URL = require("./models/url");
 
 const urlRoute = require("./routes/url");
 
@@ -11,5 +12,21 @@ connectToMongoDB("mongodb://127.0.0.1:27017/short-url").then(() => console.log('
 app.use(express.json());
 
 app.use("/url", urlRoute);
+
+app.get('/:shortId', async (req,res)=>{
+    const shortId = req.params.shortId;
+    const entry = await URL.findOneAndUpdate({
+        shortId,
+    },
+{
+    $push: {
+        visitHistory: {
+           timestamp: Date.now() 
+        },
+    },
+}
+ );
+ res.redirect(entry.redirectUrl);
+});
 
 app.listen(PORT, () => console.log(`Server started at PORT:${PORT}`));

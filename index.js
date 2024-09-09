d99653d54188d9719745a4eb979d0affc37b2cfc
mani-carboledger <mani.bhaskar@carboledger.com>
2024-09-09 15:14:03 +0530

    db connection to mongosh and post request handling for shortid generation

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const urlRoute = require("./routes/url");
 const app = express();
 const PORT = 8081;
 
+connectToMongoDB("mongodb://127.0.0.1:27017/short-url").then(() => console.log('MongoDB Connected'));
+
+app.use(express.json());
+
 app.use("/url", urlRoute);
 
 app.listen(PORT, () => console.log(`Server started at PORT:${PORT}`));

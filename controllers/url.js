78aed13cd44c3badaec5a6aa408c4cf1e50618bb
mani-carboledger <mani.bhaskar@carboledger.com>
2024-09-09 13:33:25 +0530

    models routes setup

---
+++ controllers/url.js
@@ -0,0 +1,18 @@
+const {} = require ("nanoid");
+const URL = require('../models/url');
+
+async function handleGenerateNewShortUrl(req,res) {
+    const body = req.body;
+    if(!body.url) return res.status(400).json({error: 'url is required'})
+    const shortID = nanoid(8);
+    await URL.createObjectURL({
+        shortId: shortID,
+        redirectURL: body.url,
+        visitHistory:[],
+    });
+    return res.json({id: shortID});
+}
+
+module.exports = {
+    handleGenerateNewShortUrl,
+}
